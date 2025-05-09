1f35c2b538bb006c56e03d11350cabe2dba3c564
krylov <vaness.engineer@gmail.com>
2025-05-09 22:02:55 +0300

    📦️ (project): Update config files and add base folder structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	reactStrictMode: true,
+	poweredByHeader: false,
+	async rewrites() {
+		return [
+			{
+				source: '/uploads/:path*',
+				destination: `${process.env.SERVER_URL}/uploads/:path*`
+			}
+		]
+	}
+}
 
-export default nextConfig;
+export default nextConfig
